0c043332cb96ee07f6ffb5a0346025d347beb838
Liliana Blattner <lilian14torres@gmail.com>
2021-05-08 10:03:26 -0400

    Stated creating JS variables

---
+++ src/app.js
@@ -0,0 +1,3 @@
+let apiKey = "ad95ba85cf6afff9b76905b9243ff9af";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+ console.log(apiUrl);
