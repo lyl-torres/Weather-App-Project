1a877f14d0a2fdf61794a9b65b67d9e5eba15be8
Liliana Blattner <lilian14torres@gmail.com>
2021-05-11 14:55:57 -0400

    Styled the variables and gave them classes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+function displayTemperacture(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
 let apiKey = "ad95ba85cf6afff9b76905b9243ff9af";
 let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
- console.log(apiUrl);
+
+axios.get(apiUrl).then (displayTemperature);
+ 
