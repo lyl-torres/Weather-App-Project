6d28d3f1a1ec0aca0c99cd879739ea9139ce49ac
Liliana Blattner <lilian14torres@gmail.com>
2021-05-14 11:59:50 -0400

    Created api url with coordinates to get the actual forecast

--- src/app.js
+++ src/app.js
@@ -59,6 +59,12 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "ad95ba85cf6afff9b76905b9243ff9af";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -81,6 +87,9 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+
 }
 function search(city) {
 let apiKey = "ad95ba85cf6afff9b76905b9243ff9af";
